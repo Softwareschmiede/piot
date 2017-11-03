671dad4fcde92798b36854d8406b0d3b61d95fd5
David Spanier <david.spanier@fincon.eu>
2017-11-03 14:36:21 +0100

    Init project

---
+++ index.js
@@ -0,0 +1,22 @@
+const Express = require('express');
+
+const piot = Express();
+const http = require('http').Server(piot);
+const io = require('socket.io')(http);
+
+const ESP3 = require('esp3');
+
+
+const esp = new ESP3();
+
+piot.get('/', function(req, res) {
+    res.send('<h1>Hello World</h1>');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
